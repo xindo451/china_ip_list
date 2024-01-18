e03419dc0243eb78d7ef2897e214b28a415aced1
林锌都 <45853717+xindo451@users.noreply.github.com>
2024-01-18 17:04:34 +0800

    Update make_list.sh

--- make_list.sh
+++ make_list.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
+wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 
 {
 echo "/ip firewall address-list"
 
-for net in $(cat CN.txt) ; do
+for net in $(cat china_ip_list.txt) ; do
   echo "add list=CNIP address=$net comment=CNIP"
 done
 
